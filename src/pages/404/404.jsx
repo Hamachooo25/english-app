7d4dc1a4c56abb81e8d4a335896f642645e6064a
yuta-hamasaki <yh02052002@gmail.com>
2024-05-07 21:51:56 -0700

    added 404 style

--- src/pages/404/404.jsx
+++ src/pages/404/404.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Link } from "react-router-dom"
 import Header from '../../components/header/Header'
+import "./404.css"
 
 export const Page404 = () => {
   return (
     <>
     <Header/>
-      <h1>404 NOT FOUND</h1>
+      <h1 className='notFoud'>404 NOT FOUND</h1>
       <p>The page you are looking for could not be found.</p>
       <Link to="/">Top</Link>
     </>
